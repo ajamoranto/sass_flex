db62834f57486c1d60aa5d3522d942e95eb59447
ajamoranto <30665360+ajamoranto@users.noreply.github.com>
2017-09-25 07:20:57 -0700

    added cssVal

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
+var validate = require('gulp-w3c-css');
+var gutil = require('gulp-util');
+var path = require('path');
+
 var htmlhint = require("gulp-htmlhint");
+
+var srcPath = path.join(__dirname, './assets/*.css');
+var dstPath = path.join(__dirname, './build');
+ 
+gulp.task('cssVal', function(){
+  gulp.src(srcPath)
+  .pipe(validate())
+  .pipe(gulp.dest(dstPath));
+})
+
 
 gulp.task('htmlHint', function(){
     gulp.src("./*.html")
